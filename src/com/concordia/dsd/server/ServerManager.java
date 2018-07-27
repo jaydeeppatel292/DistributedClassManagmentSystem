5ffd76fdd073fd94e838160237f68fe89dd483c0
JaydeepPatel <jaydeep.patel@taigamotors.ca>
2018-07-27 12:54:07 -0400

    added all methods to communicate with master and backup server

--- src/com/concordia/dsd/server/ServerManager.java
+++ src/com/concordia/dsd/server/ServerManager.java
@@ -3,7 +3,7 @@ package com.concordia.dsd.server;
 import com.concordia.dsd.global.cmsenum.Location;
 import com.concordia.dsd.server.generics.CenterServerImpl;
 
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Singleton ServerManager
@@ -11,8 +11,7 @@ import java.util.HashMap;
  */
 public class ServerManager {
     private static ServerManager ourInstance = new ServerManager();
-    private HashMap<Location, HashMap<Integer, CenterServerImpl>> centerServerMap = new HashMap<>();
-    private HashMap<Location, Integer> masterPortMap = new HashMap<>();
+    private HashMap<Location, List<CenterServerInfo>> centerServerMap = new HashMap<>();
 
     public static ServerManager getInstance() {
         return ourInstance;
@@ -20,6 +19,41 @@ public class ServerManager {
 
     private ServerManager() {
 
+    }
+
+
+    public void setNewMasterServer(Location location,int masterPort){
+        for(CenterServerInfo centerServerInfo : centerServerMap.get(location)){
+            if(centerServerInfo.getPort() == masterPort){
+                centerServerInfo.setMaster(true);
+            }else {
+                centerServerInfo.setMaster(false);
+            }
+        }
+
+    }
+
+    public List<CenterServerInfo> getAllMasterServer(){
+        List<CenterServerInfo> masterServerList = new ArrayList<>();
+        Iterator it = centerServerMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            for(CenterServerInfo centerServerInfo : centerServerMap.get((Location) pair.getKey())){
+                if(centerServerInfo.isMaster()){
+                    masterServerList.add(centerServerInfo);
+                }
+            }
+        }
+        return masterServerList;
+    }
+    public List<CenterServerInfo> getAllBackupServerList(Location location){
+        List<CenterServerInfo> backupServerList = new ArrayList<>();
+        for(CenterServerInfo centerServerInfo : centerServerMap.get(location)){
+            if(!centerServerInfo.isMaster()){
+                backupServerList.add(centerServerInfo);
+            }
+        }
+        return backupServerList;
     }
 
     /**
@@ -30,22 +64,67 @@ public class ServerManager {
      */
     public void addServer(Location location, int port, CenterServerImpl centerServer) {
         if (centerServerMap.get(location) == null) {
-            centerServerMap.put(location, new HashMap<>());
+            centerServerMap.put(location, new ArrayList<>());
         }
-        centerServerMap.get(location).put(port, centerServer);
+        centerServerMap.get(location).add(new CenterServerInfo(port, centerServer, location));
     }
 
     /**
-     * Get CenterServer based on Location
+     * Get CenterServerInfo based on Location
      *
      * @param location
      * @return
      */
     public CenterServerImpl getCenterServer(Location location, int port) {
-        return centerServerMap.get(location).get(port);
+        for(CenterServerInfo centerServerInfo : centerServerMap.get(location)){
+            if(centerServerInfo.getPort() == port){
+                return centerServerInfo.getCenterServerImpl();
+            }
+        }
+        return null;
     }
 
     public Integer getMasterServerPort(Location location) {
-        return masterPortMap.get(location);
+        for(CenterServerInfo centerServerInfo : centerServerMap.get(location)){
+            if(centerServerInfo.isMaster()){
+                return centerServerInfo.getPort();
+            }
+        }
+        return null;
+    }
+
+
+    class CenterServerInfo {
+        private int port;
+        private CenterServerImpl centerServerImpl;
+        private boolean isMaster;
+        private Location location;
+
+        public Location getLocation() {
+            return location;
+        }
+
+        public boolean isMaster() {
+            return isMaster;
+        }
+
+        public void setMaster(boolean master) {
+            isMaster = master;
+        }
+
+        public CenterServerInfo(int port, CenterServerImpl centerServerImpl, Location location) {
+            this.port = port;
+            this.centerServerImpl = centerServerImpl;
+            this.location = location;
+            this.isMaster = false;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public CenterServerImpl getCenterServerImpl() {
+            return centerServerImpl;
+        }
     }
 }
